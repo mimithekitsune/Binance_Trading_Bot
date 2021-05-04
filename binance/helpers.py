2e677443a7672ad6b93ea01640c3e47b1d35efe8
mimithekitsune <83654066+mimithekitsune@users.noreply.github.com>
2021-05-04 18:22:50 +0300

    Add files via upload

---
+++ binance/helpers.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+from typing import Union, Optional, Dict
+
+import dateparser
+import math
+import pytz
+
+from datetime import datetime
+
+
+def date_to_milliseconds(date_str: str) -> int:
+
+    # get epoch value in UTC
+    epoch: datetime = datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
+    # parse our date string
+    d: Optional[datetime] = dateparser.parse(date_str)
+    # if the date is not timezone aware apply UTC timezone
+    if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
+        d = d.replace(tzinfo=pytz.utc)
+
+    # return the difference in time
+    return int((d - epoch).total_seconds() * 1000.0)
+
+
+def interval_to_milliseconds(interval: str) -> Optional[int]:
+
+    seconds_per_unit: Dict[str, int] = {
+        "m": 60,
+        "h": 60 * 60,
+        "d": 24 * 60 * 60,
+        "w": 7 * 24 * 60 * 60,
+    }
+    try:
+        return int(interval[:-1]) * seconds_per_unit[interval[-1]] * 1000
+    except (ValueError, KeyError):
+        return None
+
+
+def round_step_size(quantity: Union[float, Decimal], step_size: Union[float, Decimal]) -> float:
+
+    precision: int = int(round(-math.log(step_size, 10), 0))
+    return float(round(quantity, precision))
+
+
+def convert_ts_str(ts_str):
+    if ts_str is None:
+        return ts_str
+    if type(ts_str) == int:
+        return ts_str
+    return date_to_milliseconds(ts_str)
---
+++ examples/save_historical_data.py
@@ -0,0 +1,122 @@
+import time
+import dateparser
+import pytz
+import json
+
+from datetime import datetime
+from binance.client import Client
+
+
+def date_to_milliseconds(date_str):
+
+    # get epoch value in UTC
+    epoch = datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
+    # parse our date string
+    d = dateparser.parse(date_str)
+    # if the date is not timezone aware apply UTC timezone
+    if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
+        d = d.replace(tzinfo=pytz.utc)
+
+    # return the difference in time
+    return int((d - epoch).total_seconds() * 1000.0)
+
+
+def interval_to_milliseconds(interval):
+
+    ms = None
+    seconds_per_unit = {
+        "m": 60,
+        "h": 60 * 60,
+        "d": 24 * 60 * 60,
+        "w": 7 * 24 * 60 * 60
+    }
+
+    unit = interval[-1]
+    if unit in seconds_per_unit:
+        try:
+            ms = int(interval[:-1]) * seconds_per_unit[unit] * 1000
+        except ValueError:
+            pass
+    return ms
+
+
+def get_historical_klines(symbol, interval, start_str, end_str=None):
+
+    # create the Binance client, no need for api key
+    client = Client("", "")
+
+    # init our list
+    output_data = []
+
+    # setup the max limit
+    limit = 500
+
+    # convert interval to useful value in seconds
+    timeframe = interval_to_milliseconds(interval)
+
+    # convert our date strings to milliseconds
+    start_ts = date_to_milliseconds(start_str)
+
+    # if an end time was passed convert it
+    end_ts = None
+    if end_str:
+        end_ts = date_to_milliseconds(end_str)
+
+    idx = 0
+    # it can be difficult to know when a symbol was listed on Binance so allow start time to be before list date
+    symbol_existed = False
+    while True:
+        # fetch the klines from start_ts up to max 500 entries or the end_ts if set
+        temp_data = client.get_klines(
+            symbol=symbol,
+            interval=interval,
+            limit=limit,
+            startTime=start_ts,
+            endTime=end_ts
+        )
+
+        # handle the case where our start date is before the symbol pair listed on Binance
+        if not symbol_existed and len(temp_data):
+            symbol_existed = True
+
+        if symbol_existed:
+            # append this loops data to our output data
+            output_data += temp_data
+
+            # update our start timestamp using the last value in the array and add the interval timeframe
+            start_ts = temp_data[len(temp_data) - 1][0] + timeframe
+        else:
+            # it wasn't listed yet, increment our start date
+            start_ts += timeframe
+
+        idx += 1
+        # check if we received less than the required limit and exit the loop
+        if len(temp_data) < limit:
+            # exit the while loop
+            break
+
+        # sleep after every 3rd call to be kind to the API
+        if idx % 3 == 0:
+            time.sleep(1)
+
+    return output_data
+
+
+symbol = "ETHBTC"
+start = "1 Dec, 2017"
+end = "1 Jan, 2018"
+interval = Client.KLINE_INTERVAL_30MINUTE
+
+klines = get_historical_klines(symbol, interval, start, end)
+
+# open a file with filename including symbol, interval and start and end converted to milliseconds
+with open(
+    "Binance_{}_{}_{}-{}.json".format(
+        symbol,
+        interval,
+        date_to_milliseconds(start),
+        date_to_milliseconds(end)
+    ),
+    'w'  # set file write mode
+) as f:
+    f.write(json.dumps(klines))
